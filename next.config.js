8006594903acab76222385f9dd0ec2d8fbfd448e
Dan Pietsch <drpietsch14@gmail.com>
2023-01-12 11:22:38 -0500

    added ignore of eslint errors during builds

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   reactStrictMode: true,
   // experimental: { appDir: true },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
